8c53361ceca692bd3a4d9e61d67d7c4408d65ee1
Jess Scheuring <jscheuring@gmail.com>
2016-03-02 19:06:34 -0600

    refactors to serve any asset requested from public folder

--- index.js
+++ index.js
@@ -1,30 +1,45 @@
 var http = require('http');
 var fs = require('fs');
 
+const encodingMap = {
+  '.ico': null,
+  '.css': {encoding: 'utf8'},
+  '.js' : {encoding: 'utf8'}
+};
+
+const contentTypeMap = {
+  '.ico' : 'image/icon',
+  '.css' : 'text/css',
+  '.js' : 'text/javascript'
+}
+
+var cachedPaths = [];
+
+function serveFile(data, err, fileContents) {
+  if (err) {
+    data.response.writeHead(404);
+    data.response.end();
+  } else {
+    data.response.writeHead(200, {'Content-Type': contentTypeMap[data.ext]})
+    data.response.end(fileContents);
+  }
+}
+
 function handleRequest(request, response) {
   var path = request.url;
-  if (path === '/favicon.ico') {
-    fs.readFile('public/favicon.ico', function(err, data) {
-      response.setHeader('Content-Type', 'image/icon')
-      response.end(data);
-    });
-  } else if (path.match('/styles/')) {
-    fs.readFile('public'+path, {encoding: 'utf8'}, function(err, data) {
-      if (err) {
-        response.writeHead(404);
+  var ext = path.match(/(\..+)/) ? path.match(/(\..+)/)[0] : undefined;
+  var encoding = encodingMap[ext] === undefined ? {encoding: 'utf8'} : encodingMap[ext]
+  var data = {response: response, ext: ext};
+  if (ext) {
+    cachedPaths.forEach(function(p) {
+      if (path.indexOf(p) > -1) {
+        path = path.substr(p.length);
       }
-      response.setHeader('Content-Type', 'text/css')
-      response.end(data);
     });
+    fs.readFile('public' + path, encoding, serveFile.bind(null, data))
   } else {
-    path = path === '/' ? '/index.html' : path.match('.html') ? path : path + '.html';
-    fs.readFile('public'+path, {encoding: 'utf8'}, function(err, data) {
-      if (err) {
-        response.writeHead(404);
-        response.end('Sorry, could not find that');
-      }
-      response.end(data);
-    });
+    cachedPaths.push(path.substr(0, path.lastIndexOf('/')));
+    fs.readFile('public/index.html', {encoding: 'utf8'}, serveFile.bind(null, data));
   }
 };
 
